a769c6c169f5137ca48412024693e026259900d7
naritomi-toru <naritomi-toru@dmm.com>
2023-07-06 23:08:43 +0900

    Initial commit

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 import discord
-from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+token = os.getenv("DISCORD_TOKEN") #Your TOKEN
+client = discord.Client()
 
-@bot.event
+@client.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print(f'We have logged in as {client.user}')
 
-@bot.command()
-async def schedule(ctx, from_date, to_date):
-    # from_dateとto_dateの間の水曜日、土曜日、日曜日の日付を取得
-    # ここで、from_dateとto_dateの指定された日付を処理して、目的の日付のリストを作成する必要があります
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    wednesdays = []  # 水曜日の日付リスト
-    saturdays = []  # 土曜日の日付リスト
-    sundays = []    # 日曜日の日付リスト
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-    # 出欠確認メッセージの作成
-    message = "出欠確認\n"
-    for date in wednesdays:
-        message += f"{date.strftime('%Y-%m-%d')} (水曜日): ✅\n"
-    for date in saturdays:
-        message += f"{date.strftime('%Y-%m-%d')} (土曜日): ✅\n"
-    for date in sundays:
-        message += f"{date.strftime('%Y-%m-%d')} (日曜日): ✅\n"
-
-    # スタンプを送信して出欠確認
-    await ctx.send(message)
-    await ctx.message.add_reaction("✅")
-
-# Discordボットを起動
-bot.run('YOUR_BOT_TOKEN')
+client.run(token)
 
