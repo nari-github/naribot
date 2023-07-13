0d0714a75b367595a01ab15115cedabbc5d46a41
nari-github <138776219+nari-github@users.noreply.github.com>
2023-07-13 21:32:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ async def on_ready():
 # メッセージを受信した時に呼ばれる
 @client.event
 async def on_message(message):
+    if message.author.bot:
+        await message.add_reaction(":o:")
+        await message.add_reaction(":small_red_triangle:")
+        await message.add_reaction(":x:")
+        return
+        
     # 自分のメッセージを無効
     if message.author == client.user:
         await message.add_reaction(":o:")
@@ -32,9 +38,11 @@ async def on_message(message):
     # メッセージが"$hello"で始まっていたら"Hello!"と応答
     if message.content.startswith('$neko'):
         await message.channel.send('にゃーん(=^・^=)')
+        return
 
     if message.content.startswith('/レギマ'):
         await message.channel.send('7/30（日）')
+        return
 
 # クライアントの実行
 client.run(token)
