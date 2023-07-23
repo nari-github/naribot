56c4aa263f1aabb15bde20058d79334d6bc2a25c
nari-github <138776219+nari-github@users.noreply.github.com>
2023-07-23 10:25:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,90 @@
 import discord
 import os
-import time
-import socket
 import sys
-import requests
+import datetime
+import calendar
 
-allow_ip = os.getenv("ALLOW_IP")
-
-#host = socket.gethostname()
-#ip = socket.gethostbyname(host)
-my_ip = requests.get('https://ifconfig.me')
-
-if allow_ip != my_ip.text:
-    sys.stdout.write("NG:{}".format(my_ip.text))
-    sys.exit()
-
-sys.stdout.write("OK:{}".format(my_ip.text))
-
-token = os.getenv("DISCORD_TOKEN") #Your TOKEN
+token = os.getenv('DISCORD_TOKEN')  #Your TOKEN
 
 # インテントの生成
 intents = discord.Intents.default()
 intents.message_content = True
 
 # クライアントの生成
-client = discord.Client(intents=intents)
+client = discord.Client(intents = intents)
 
 # discordと接続した時に呼ばれる
 @client.event
 async def on_ready():
-    sys.stdout.write("login OK")
+  print('login OK')
 
 # メッセージを受信した時に呼ばれる
 @client.event
 async def on_message(message):
-    if message.author.bot:
-        return
-        
-    # 自分のメッセージを無効
-    if message.author == client.user:
-        return
+  # 自分のメッセージを無効
+  if message.author.bot:
+    # https://emojipedia.org/
+    await message.add_reaction('⭕')
+    await message.add_reaction('🔺')
+    await message.add_reaction('❌')
+    return
 
-    # メッセージにリアクション追加
-    # emoji ="👍"
-    #await message.add_reaction("<:watashi_okage:989196108486033449>")
-    await message.add_reaction("⭕")
-    await message.add_reaction("🔺")
-    await message.add_reaction("❌")
+  # メッセージにリアクション追加
+  # await message.add_reaction("<:watashi_okage:989196108486033449>")
 
-    # メッセージが"$hello"で始まっていたら"Hello!"と応答
-    if message.content.startswith('$neko'):
-        await message.channel.send('にゃーん(=^・^=)')
-        return
+  # メッセージに反応
+  if message.content.startswith('/match'):
+    # 設定値
+    week_list = ['月', '火', '水', '木', '金', '土', '日', ]
+    match_week_list = [2, 5, 6] # レギマ曜日（水、土、日）
+    day = 1 # 通知開始日
+    league_flg = False
+    prefix = ''
+    has_league_day = False
 
-    if message.content.startswith('/レギマ'):
-        time.sleep(1)
-        await message.channel.send('7/20（日）')
-        return
+    # 引数取得
+    args = message.content.split('/match ', 1)
+    if args[1:2]:
+      month = args[1].split(' ')[0]
+      if args[1].split(' ')[1:2]:
+        start = args[1].split(' ')[1].split('-')[0]
+        end = args[1].split(' ')[1].split('-')[1]
+        has_league_day = True
+    else:
+      month = datetime.datetime.now().month
+
+    date_obj = datetime.datetime.now()
+    date_obj = date_obj.replace(month = int(month))
+    day_count = calendar.monthrange(date_obj.year, date_obj.month)[1]
+
+    for i in range(day_count):
+      date_obj = date_obj.replace(day = day)
+
+      # リーグ期間判定
+      if has_league_day:
+        if int(start) == day:
+          prefix = '＝＝＝＝＝ここからレギリ＝＝＝＝＝\n'
+          league_flg = True
+  
+        if int(end) < day:
+          if league_flg == True:
+            prefix = '＝＝＝＝＝ここまでレギリ＝＝＝＝＝\n'
+            league_flg = False
+  
+      # リーグ期間中は毎日通知
+      if league_flg == True:
+        await message.channel.send(prefix + date_obj.strftime('%m/%d') + '（' + week_list[date_obj.weekday()] + '）')
+        prefix = ''
+      # 特定曜日のみ通知
+      elif date_obj.weekday() in match_week_list:
+        await message.channel.send(prefix + date_obj.strftime('%m/%d') + '（' + week_list[date_obj.weekday()] + '）')
+        prefix = ''
+      
+      day = day + 1
+    return
+
+  if message.content.startswith('/チャーミィ'):
+    await message.channel.send('呼んだ？')
 
 # クライアントの実行
 client.run(token)
